b8e216022813b614c7023b4851256c182b25787b
Zahra Mahoor <zahra@ip0af50704.int.uvm.edu>
2017-05-08 13:57:21 -0400

    changed population.py to calculate relative fitness for novely exploration

--- pyrosim/body.py
+++ pyrosim/body.py
@@ -9,7 +9,7 @@ import random
 
 class BODY:
 
-    def __init__(self):
+    def __init__(self, maxDepth):
 
         self.numObjects = 0
 
@@ -17,7 +17,7 @@ class BODY:
 
         self.numSensors = 0
 
-        self.root = NODE(None,0,c.maxDepth,1,0.0,0.0,0,0,0)
+        self.root = NODE(None,0,maxDepth,1,0.0,0.0,0,0,0)
 
         self.head_ID = 0
 
@@ -37,15 +37,15 @@ class BODY:
 
         if(whatToMaximize == c.maximizeMovementAndHeight):
 
-            return self.Sum_Joint_Diff()*self.Head_Z_Position()
+            return -(self.Sum_Joint_Diff()*self.Head_Z_Position())
 
         if(whatToMaximize == c.maximizeHeight):
 
-            return self.Head_Z_Position()
+            return -self.Head_Z_Position()
 
         if(whatToMaximize == c.maximizeDistance):
 
-            return self.Head_X_Position()
+            return -self.Head_X_Position()
 
         else:
             print 'unknown fitness function ' + whatToMaximize
@@ -55,6 +55,12 @@ class BODY:
     def Get_Sensor_Data_From_Simulator(self,simulator):
 
         self.root.Get_Sensor_Data_From_Simulator(simulator)
+
+    def Get_Head_Trajectory(self, simulator):
+        
+        if self.root.object.positionSensor:
+
+            return self.root.object.positionSensor.values
 
     def Mutate(self):
 
