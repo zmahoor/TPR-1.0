9aae97ce1adf757aa4847193c381af0a9f08dd40
Zahra Mahoor <zahra@ip0af50481.int.uvm.edu>
2017-05-30 11:14:48 -0400

    cleaned up steady state

--- pyrosim/steadyState.py
+++ pyrosim/steadyState.py
@@ -19,101 +19,85 @@ from settings import *
 
 colorIndex = 0
 morphologyIndex = 0
-numAliveIndividuals = 20
 
-MORPHOLOGY_DURATION =  1 * 60
+SUB_POPULATION_SIZE = 20
+MORPHOLOGY_DURATION = 5 * 60
 INDIVIDUAL_DURATION = 30
+REWARD_WINDOW_W = 950
+REWARD_WINDOW_H = 280
 
 currentColor = validColors[colorIndex % len(validColors)]
 robotType = validRobots[morphologyIndex % len(validRobots)]
 
 mydatabase = DATABASE()
 
-window = PYGAMEWRAPPER(width=950,height=280)
+window = PYGAMEWRAPPER(width=REWARD_WINDOW_W, height=REWARD_WINDOW_H)
 
 currentCommand = {'cmdCount':0, 'cmdTxt':"roll"}
 
-def store_Sensors_To_File(individual, currentTime):
+def Store_Sensors_To_File(individual, currentTime):
 
-    sensorValues = individual.Get_Raw_Sensors()
-
-    id = individual.id
-
+    # get directory
     path = "../sensors/"+ str(currentTime.year) + "/" + str(currentTime.month)+\
         "/" + str(currentTime.day)
-
     # print path
-
     if not os.path.exists(path):
-
         os.makedirs(path)
 
-    currentTime = currentTime.strftime("%Y-%m-%d %H:%M:%S")
-
+    # create file
+    currentTime = currentTime.strftime("%Y-%m-%d %H-%M-%S")
     path += '/robot_' + str(individual.id) + "_" + currentTime + ".dat"
-
+    sensorValues = individual.Get_Raw_Sensors()
     with open( path , 'wb' ) as f:
-
         pickle.dump(sensorValues, f)
 
+def Store_Controller_To_File(individual):
 
-def store_Controller_To_File(individual):
-
-    id = individual.id
-
+    # get directory
     path = "../controllers/"+ robotType
-
     if not os.path.exists(path):
-
         os.makedirs(path)
 
+    # get filename and extension
     path += '/robot_' + str(individual.id) + ".dat"
-
     with open( path , 'wb' ) as f:
-
         pickle.dump(individual, f)
 
-
-def load_Controller_From_File(robotID):
+def Load_Controller_From_File(robotID):
 
     brainPath = "../controllers/"+ robotType +"/robot_"+ str(robotID) +".dat" 
 
-    print brainPath
+    if not os.path.isfile(brainPath): 
+        return None
 
-    if not os.path.isfile(brainPath): return None
+    return Read_File(brainPath)
 
-    individual = None
-
-    with open(brainPath,'r') as f:
-
-        individual = pickle.load(f)
-
-    return individual
-
-
-def load_From_Diversity_Pool(robotType):
+def Load_From_Diversity_Pool(robotType):
 
     path = "../diversity_pool/"+ robotType + "/*.dat" 
 
-    brainPaths = list()
-
-    for f in glob.iglob(path): brainPaths.append(f)
-
-    print path, robotType, len(brainPaths)
+    brainPaths = list(glob.iglob(path))
 
     randomIndex = np.random.randint(low=0, high=len(brainPaths))
+    if not os.path.isfile(brainPaths[randomIndex]): 
+        return None
 
-    if not os.path.isfile(brainPaths[randomIndex]): return None
+    return Read_File(brainPaths[randomIndex])
 
-    individual = None
+def Read_File(filePath):
 
-    with open(brainPaths[randomIndex],'r') as f:
-
+    try:
+        f = open(filePath,'r')
         individual = pickle.load(f)
+        f.close
+        
+        print "Successful loading ", filePath
+        return individual
+    except:
+        print "Failed loading ", filePath 
+        return None
 
-    return individual
-
-def draw_Reinforcment_Window():
+def Draw_Reinforcment_Window():
 
     global currentCommand
     global currentColor
@@ -128,7 +112,6 @@ def draw_Reinforcment_Window():
     window.Draw_Text(" if the current robot is obeying the command", x= 130, y=myy)
     window.Draw_Text("["+ cmdTxt +"].", x= 800, y=myy, color='brown')
 
-    
     myy += 40
     window.Draw_Text("Type", x= 10, y= myy)
     window.Draw_Text("!"+ currentColor[0] + "n", x= 80, y=myy, color=currentColor)
@@ -136,88 +119,89 @@ def draw_Reinforcment_Window():
     window.Draw_Text("["+ cmdTxt +"].", x= 800, y=myy, color='brown')
 
     myy += 40
-    
     window.Draw_Text("Type", x= 10, y=myy)
     window.Draw_Text("!"+ currentColor[0] + "l", x= 80, y=myy, color=currentColor)
     window.Draw_Text(" if you LIKE the current robot." , x= 130, y=myy)
     
     myy += 40
-
     window.Draw_Text("Type", x= 10, y=myy)
     window.Draw_Text("!"+ currentColor[0] + "d", x= 80, y=myy, color=currentColor)
     window.Draw_Text(" if you DISLIKE the current robot." , x= 130, y=myy)
 
     myy += 60
-
     window.Draw_Text("Need help: type", x= 700, y=myy) 
     window.Draw_Text("?", x= 920, y=myy, color='brown')
 
     window.Refresh()
 
-def select_Random_Individual(pop):
+def Select_Random_Individual(popSize):
+    return np.random.randint(popSize)
+
+def Compete_While_Waiting_For(pop, ignoreIndex):
 
     pop_len = len(pop)
+    if (pop_len <= 2) : 
+        return None
 
-    return np.random.randint(pop_len)
+    while True:
+        ind1 = Select_Random_Individual(len(pop))
+        if ind1 != ignoreIndex:
+            break
 
-def compete_While_Waiting_For(pop, ignoreIndex):
-
-    pop_len = len(pop)
-
-    if (pop_len <= 2) : return None
-
-    ind1 =  np.random.randint(pop_len)
-    ind2 =  np.random.randint(pop_len)
-
-    while ind1 == ignoreIndex:
-        ind1 =  np.random.randint(pop_len)
-
-    while ind2 == ind1 or ind2 == ignoreIndex: 
-        ind2 =  np.random.randint(pop_len)
-
+    while True:
+        ind2 = Select_Random_Individual(len(pop))
+        if ind2 != ind1 and ind2 != ignoreIndex:
+            break
+    print "Competing controllers: ", pop[ind1]['robotID'], " and " , pop[ind2]['robotID']
     print pop[ind1]
     print pop[ind2]
 
-    return compete_Based_On_Dominance(pop[ind1], pop[ind2])
+    return Compete_Based_On_Dominance(pop[ind1], pop[ind2])
 
-def compete_Based_On_Dominance(individual1, individual2):
+def Compete_Based_On_Dominance(individual1, individual2):
 
-    if dominance(individual1, individual2):
-        winner, loser = individual1, individual2
-
-    elif dominance(individual2, individual1):
+    winner, loser = individual1, individual2
+    if Dominance(individual2, individual1):
         winner, loser = individual2, individual1
-
-    else:
+    elif not Dominance(individual1, individual2):
         return None
+    print "Winner is: ", winner['robotID'], " loser is: ", loser['robotID']
 
     mydatabase.Kill_Robot(loser['robotID'])
 
-    newInd = load_Controller_From_File(winner['robotID'])
-
-    print("winner: ", winner['robotID'])
-
+    newInd = Load_Controller_From_File(winner['robotID'])
     if newInd == None:  
-
         mydatabase.Kill_Robot(winner['robotID'])
-
     else:
         newInd.Mutate()
-
     return newInd
 
-def dominance(individual1, individual2):
+def Dominance(individual1, individual2):
 
     notShownMore    = individual1['numEvals'] <= individual2['numEvals']
     likedMore       = individual1['totalLikeability'] > individual2['totalLikeability']
     moreObedient    = individual1['totalFitness'] > individual2['totalFitness']
+    return notShownMore and likedMore and moreObedient
 
-    if notShownMore and likedMore and moreObedient:
-        return True
-    else:
-        return False
+def Add_New_Robot(newIndividual):
 
-def morphology_Cycle(morphology_timer):
+    robotID = mydatabase.Add_Robot(robotType)
+    newIndividual.Set_ID(robotID)
+    Store_Controller_To_File(newIndividual)
+
+def Initialize_Sub_Population(numToBeFilled):
+
+    print "Empty slots in sub population of ", robotType, " is: ", numToBeFilled
+    while numToBeFilled > 0 :
+
+        newIndividual = Load_From_Diversity_Pool(robotType)
+        if newIndividual == None:
+            newIndividual = INDIVIDUAL(0, robotType)
+        Add_New_Robot(newIndividual)
+
+        numToBeFilled -= 1
+
+def Morphology_Cycle(morphologyTimer):
 
     global robotType
     global currentCommand
@@ -225,78 +209,46 @@ def morphology_Cycle(morphology_timer):
     global colorIndex
     global morphologyIndex
 
-    while not morphology_timer.Time_Elapsed():
-
-        currentColor = validColors[colorIndex % len(validColors)]
+    while not morphologyTimer.Time_Elapsed():
 
         aliveIndividuals = mydatabase.Fetch_Alive_Robots(robotType)
 
-        temp = mydatabase.Fetch_Popular_Command()
+        Initialize_Sub_Population(SUB_POPULATION_SIZE - len(aliveIndividuals))
 
-        if temp != None: currentCommand = temp
+        index = Select_Random_Individual(len(aliveIndividuals))
 
-        # print currentColor, len(aliveIndividuals), numAliveIndividuals
+        randomIndividual = Load_Controller_From_File(aliveIndividuals[index]['robotID'])
 
-        if len(aliveIndividuals) < numAliveIndividuals:
+        if randomIndividual == None:
+            mydatabase.Kill_Robot(aliveIndividuals[index]['robotID'])
+            continue
 
-            if np.random.uniform(0, 1) >= 0:
+        currentColor = validColors[colorIndex % len(validColors)]
 
-                newIndividual = INDIVIDUAL(0, robotType)
+        Draw_Reinforcment_Window()
 
-            else:
+        currentTime = datetime.datetime.now()
 
-                newIndividual = load_From_Diversity_Pool(robotType)
+        popularCommand = mydatabase.Fetch_Popular_Command()
+        currentCommand = popularCommand if popularCommand != None else currentCommand
 
-            robotID = mydatabase.Add_Robot(robotType)
+        mydatabase.Add_Command_To_Display(aliveIndividuals[index]['robotID'],
+            currentCommand['cmdTxt'], currentColor[0], currentTime)
 
-            print "newIndividual", robotID
+        print "Displaying controller ", randomIndividual.id, " of type ", robotType
+        randomIndividual.Set_Color(currentColor)
+        randomIndividual.Start_Evaluate(False, False, [1.0])
 
-            newIndividual.Set_ID(robotID)
+        newIndividual = Compete_While_Waiting_For(aliveIndividuals, index)
+        if newIndividual != None:
+            Add_New_Robot(newIndividual)
 
-            store_Controller_To_File(newIndividual)
+        randomIndividual.Wait_For_Me()
 
-        else:
-
-            index = select_Random_Individual(aliveIndividuals)
-
-            randomIndividual = load_Controller_From_File(aliveIndividuals[index]['robotID'])
-
-            print "randomIndividual", aliveIndividuals[index]['robotID']
-
-            if randomIndividual == None:
-
-                mydatabase.Kill_Robot(aliveIndividuals[index]['robotID'])
-
-                continue
-
-            draw_Reinforcment_Window()
-
-            currentTime = datetime.datetime.now()
-
-            mydatabase.Add_Command_To_Display(aliveIndividuals[index]['robotID'],
-                currentCommand['cmdTxt'], currentColor[0], currentTime)
-
-            randomIndividual.Set_Color(currentColor)
-
-            randomIndividual.Start_Evaluate(False, False, [1.0])
-
-            newIndividual = compete_While_Waiting_For(aliveIndividuals, index)
-
-            if newIndividual != None:
-
-                robotID = mydatabase.Add_Robot(robotType)
-
-                print("child: ", robotID)
-                
-                newIndividual.Set_ID(robotID)
-
-                newIndividual.store_Controller_To_File()
-
-            randomIndividual.Wait_For_Me()
-
-            store_Sensors_To_File(randomIndividual, currentTime)
+        Store_Sensors_To_File(randomIndividual, currentTime)
 
         colorIndex += 1
+        print
 
 def main(argv):
 
@@ -308,11 +260,11 @@ def main(argv):
     while True:
         print("G: ", generation)
 
-        morphology_timer = TIMER(MORPHOLOGY_DURATION)
+        morphologyTimer = TIMER(MORPHOLOGY_DURATION)
 
         robotType = validRobots[morphologyIndex % len(validRobots)]
 
-        morphology_Cycle(morphology_timer)
+        Morphology_Cycle(morphologyTimer)
 
         morphologyIndex += 1
 
