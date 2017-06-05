9d302e7d6b77fb3c768bbdfacbfd798edb1fc207
Zahra Mahoor <zahra@ip0af50481.int.uvm.edu>
2017-06-05 14:42:29 -0400

    added the programs that draw/update for commandboard, leaderboard, and currentcommand. fixed a bug in synapses.py

--- bots/pygameWrapper.py
+++ bots/pygameWrapper.py
@@ -2,47 +2,110 @@ import pygame
 
 class PYGAMEWRAPPER:
 
-    def __init__(self,width=800,height=240):
-
-        pygame.init()
-
-        size = width,height
-
-        self.screen = pygame.display.set_mode(size)
-
-        self.myfont = pygame.font.Font("RobotoCondensed-Regular.ttf",26)
-
-    def Get_Color(self, color='black'):
-
-        if color=='black': return (0, 0, 0)
-        if color=='green': return (0, 128, 0)
-        if color=='red': return (255, 0, 0)
-        if color=='blue': return (0, 0, 255)
-        if color=='yellow': return (255, 255, 0)
-        if color=='orange': return (255, 165, 0)
-        if color=='purple': return (255, 0, 255)
-        if color=='cyan': return (0, 255, 255)
-        if color=='brown': return (102, 51, 0)
-
-    def Draw_Text(self, textString , x = 10 , y = 10, color ='black' ):
         
-        label = self.myfont.render(textString, 1, self.Get_Color(color))
+                
+        def __init__(self,width=800,height=240):
 
-        textrect = label.get_rect()
+                #initialize pygame modules
+		pygame.init()
 
-        textrect.left = self.screen.get_rect().left + x
+                #set size
+		size = width,height
+		
+                self.size = width, height
 
-        textrect.top = self.screen.get_rect().top + y
+                #set screen
+		self.screen = pygame.display.set_mode(size)
 
-        self.screen.blit(label, textrect)
+                #define font to be used
+		self.myfont = pygame.font.Font("RobotoCondensed-Regular.ttf",17)
 
-    def Refresh(self):
+	def Draw_Text(self, textString , x = 10 , y = 10, color = 'BLACK' ):
 
-        pygame.display.flip()
+                #get color
+                col = self.Get_Color(color)
 
-    def Wipe(self):
+                #set area for text to be drawn on
+		label = self.myfont.render(textString, 1, col)
 
-        backgroundColor = 255, 255, 255
+                #get where the text is
+        	textrect = label.get_rect()
 
-        self.screen.fill(backgroundColor)
-    
+                textrect.left = self.screen.get_rect().left + x
+
+		textrect.top = self.screen.get_rect().top + y
+
+                #draw text
+        	self.screen.blit(label, textrect)
+
+	def Refresh(self):
+
+                #refresh screen
+		pygame.display.flip()
+
+	def Wipe(self):
+
+                #set background
+		backgroundColor = 255, 255, 255
+
+		self.screen.fill(backgroundColor)
+	
+	def Draw_Line(self, x1, y1, x2, y2, color = 'BLACK', thickness = 1):
+
+                #get color
+                col = self.Get_Color(color)
+
+                #draw line
+                line = pygame.draw.line(self.screen, col, (x1,y1), (x2,y2), thickness)
+
+        def Draw_Rect(self, x1, y1, w, h, color = 'RED'):
+
+                #get color
+                col = self.Get_Color(color)
+
+                #set rectangle dimensions
+                rect = (x1, y1, w, h)
+
+                #draw rect
+                shape = pygame.draw.rect(self.screen, col, rect, 0)
+
+        def Get_Color(self, color):
+
+                #define RGB values for common colors
+                colors = {'BLACK'       : (  0,   0,   0),
+                          'WHITE'       : (255, 255, 255),
+                          'RED'         : (255,   0,   0),
+                          'GREEN'       : (  0, 255,   0),
+                          'BLUE'        : (  0,   0, 255),
+                          'GRAY'        : (123, 123, 123),
+                          'LIGHTGRAY'   : (150, 150, 150),
+                          'DARKGRAY'    : ( 75,  75,  75),
+                          'PINK'        : (255,  10, 190),
+                          'LIGHTBLUE'   : (  0, 192, 255),
+                          'LIGHTGREEN'  : (100, 255,   0),
+                          'DARKGREEN'   : ( 11, 128,  11),
+                          'VIOLET'      : ( 70,   0, 130),
+                          'GOLD'        : (255, 192,   0),
+                          'ORANGE'      : (255, 165,   0),
+                          'PURPLE'      : (255, 0  , 255),
+                          'CYAN'        : (0, 255  , 255),
+                          'BROWN'       : (102, 51 ,   0),
+                          'YELLOW'      : (255, 255,   0)}
+
+                #get color
+                if color in colors:
+                        return colors.get(color)
+                #return BLACK if name is not found
+                else:
+                        return colors.get('BLACK')
+                
+
+        def Quit(self):
+
+                #quits out of pygame and window
+                pygame.quit()
+
+                exit()
+                
+
+                
---
+++ bots/table.py
@@ -0,0 +1,86 @@
+import pygame
+from pygameWrapper import PYGAMEWRAPPER
+
+class TABLE:
+
+    def __init__(self, window, width, height):
+
+        #define screen to draw on
+        self.screen = window.screen
+
+        #get screen dimensions
+        self.w = width
+        self.h = height
+
+        #set window
+        self.window = window
+
+    def Update(self, new, bottom):
+
+        #get size of newList that populates table (if not empty or None)
+        try:
+            size = len(new)
+        except TypeError as e:
+            size = 0
+
+        #define BLACK as RGB
+        BLACK = 0,0,0
+
+        #set window 
+        window = self.window
+
+        #set screen
+        screen = self.screen
+
+        #draw lines that define the table
+        pygame.draw.line(screen, BLACK, (.12*self.w,0), (.12*self.w,self.h*12.0/13.0), 1)
+
+        pygame.draw.line(screen, BLACK, (.625*self.w,0), (.625*self.w,self.h*12.0/13.0), 1)
+
+        pygame.draw.line(screen, BLACK, (.8*self.w, 0), (.8*self.w, self.h*12.0/13.0), 1)
+
+        pygame.draw.line(screen, BLACK, (.9*self.w, 0), (.9*self.w, self.h*12.0/13.0), 1)
+        
+        pygame.draw.line(screen, BLACK, (0, self.h/12.0), (self.w, self.h/12.0), 3)
+        
+        for i in range (2, 12):
+            
+            pygame.draw.line(screen, BLACK, (0, i*self.h/13.0), (self.w, i*self.h/13.0), 1)
+
+            #add ranks for 1-10
+            window.Draw_Text(str(i-1), x = 5, y = ((i-.5)*self.h/13.0))
+        
+        pygame.draw.line(screen, BLACK, (0, (11*self.h/13.0)), (self.w, (11*self.h/13.0)), 2)
+
+        pygame.draw.line(screen, BLACK, (0, (12*self.h/13.0)), (self.w, (12*self.h/13.0)), 2)
+
+        for i in range(0,size):
+
+            #name is the command name or username, depending on input
+            name = new[i][0]
+
+            #score is the learnability or user score
+            score = new[i][1]
+
+            #capitalize name if exists to enhance readability
+            if name != None:
+                
+                name = name.upper()
+
+            #draw name and score to the table
+            if score != None and name != None:
+                window.Draw_Text(name, x = self.w*.13, y = ((i+1.5)*self.h/13.0))
+                window.Draw_Text(str(int(score)), x = self.w*.635, y = ((i+1.5)*self.h/13.0))
+
+        #get name of recent for bottom row
+        bt = bottom[0]
+
+        #capitalize the name if exists
+        if bt != None:
+            
+            bt = bt.upper()
+
+        #draw the name and score to the screen in the bottom row 
+        window.Draw_Text(bt, x = self.w*.13, y = 11.5*self.h/13.0)  
+        window.Draw_Text(str(int(bottom[1])), x = self.w*.635, y = 11.5*self.h/13.0) 
+        
