ed01cb11abc36b39bf170375c768fcdbaca7e241
Zahra Mahoor <zahra@ip0af50481.int.uvm.edu>
2017-06-29 10:19:48 -0400

    commandboard leaderboard table.py

--- bots/table.py
+++ bots/table.py
@@ -6,14 +6,16 @@ class TABLE:
     def __init__(self, window, width, height):
 
         #define screen to draw on
-        self.screen = window.screen
+        self.SCREEN = window.screen
 
         #get screen dimensions
-        self.w = width
-        self.h = height
+        self.WIDTH = width
+        self.HEIGHT = height
 
         #set window
-        self.window = window
+        self.WINDOW = window
+        
+        self.USERS = 5
 
     def Update(self, new, bottom):
         
@@ -23,38 +25,35 @@ class TABLE:
         except TypeError as e:
             size = 0
 
-        #define BLACK as RGB
+        #define BLACK in RGB
         BLACK = 0,0,0
 
         #set window 
-        window = self.window
+        window = self.WINDOW
 
         #set screen
-        screen = self.screen
-
+        screen = self.SCREEN
+	
+	
+	
         #draw lines that define the table
         
         
-        pygame.draw.line(screen, BLACK, (0, self.h/12.0), (self.w, self.h/12.0), 3)
+        pygame.draw.line(screen, BLACK, (0, self.HEIGHT/12.0), (self.WIDTH, self.HEIGHT/12.0), 3)
         
-        for i in range (2, 12):
+        top_rect = self.HEIGHT/12.0
+        height_rect = ((11.0/12.0) * self.HEIGHT) / (self.USERS+2)
+        
+        for i in range (0, self.USERS):
 
             #add ranks for 1-10
             if i%2 == 0:
                 col = 'LIGHTBLUE'
             else:
                 col = 'TAN'
-            window.Draw_Rect(0, (i-.9)*self.h/13.0, self.w, (i+.1)*self.h/13.0, color = col)
-            window.Draw_Text(str(i-1), x = 5, y = ((i-.5)*self.h/13.0))
-        
-        pygame.draw.line(screen, BLACK, (0, (11*self.h/13.0)), (self.w, (11*self.h/13.0)), 2)
-
-        pygame.draw.line(screen, BLACK, (0, (12*self.h/13.0)), (self.w, (12*self.h/13.0)), 2)
-
-
-        
-        for i in range(0,size):
-
+            window.Draw_Rect(0, top_rect, self.WIDTH, height_rect, color = col)
+            window.Draw_Text(str(i+1), x = 4, y = top_rect + (.4*height_rect))
+            
             #name is the command name or username, depending on input
             name = new[i][0]
 
@@ -68,8 +67,22 @@ class TABLE:
 
             #draw name and score to the table
             if score != None and name != None:
-                window.Draw_Text(name, x = self.w*.13, y = ((i+1.5)*self.h/13.0))
-                window.Draw_Text(str(int(score)), x = self.w*.635, y = ((i+1.5)*self.h/13.0))
+                window.Draw_Text(name, x = self.WIDTH*.15, y = top_rect + (.4*height_rect))
+                window.Draw_Text(str(int(score)), x = self.WIDTH*.7, y = top_rect + (.4*height_rect))
+            
+            
+            
+            top_rect = top_rect + height_rect
+        
+        pygame.draw.line(screen, BLACK, (0, (11*self.HEIGHT/(self.USERS+3))), (self.WIDTH, (11*self.HEIGHT/(self.USERS+3))), 2)
+
+        pygame.draw.line(screen, BLACK, (0, (12*self.HEIGHT/(self.USERS+3))), (self.WIDTH, (12*self.HEIGHT/(self.USERS+3))), 2)
+
+
+        
+        
+
+            
 
         #get name of recent for bottom row
         bt = bottom[0]
@@ -80,5 +93,5 @@ class TABLE:
             bt = bt.upper()
 
         #draw the name and score to the screen in the bottom row 
-        window.Draw_Text(bt, x = self.w*.13, y = 11.5*self.h/13.0)  
-        window.Draw_Text(str(int(bottom[1])), x = self.w*.635, y = 11.5*self.h/13.0) 
+        window.Draw_Text(bt, x = self.WIDTH*.13, y = 11.5*self.HEIGHT/13.0)  
+        window.Draw_Text(str(int(bottom[1])), x = self.WIDTH*.635, y = 11.5*self.HEIGHT/13.0) 
