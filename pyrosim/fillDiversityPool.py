07a7a2d407803fdab5bef6edf11c1355fe7c3791
Zahra Mahoor <zahra@ip0af50481.int.uvm.edu>
2017-07-03 11:53:49 -0400

    fillDiversityPool

---
+++ pyrosim/fillDiversityPool.py
@@ -0,0 +1,89 @@
+# parallel hill climber plus novelty search to create diverse behaviours
+from pyrosim import PYROSIM
+import numpy as np
+import random
+from copy import deepcopy
+import pickle
+import argparse
+
+from individual import INDIVIDUAL
+from robot import ROBOT
+from population import POPULATION
+
+def main(args):
+
+    robotType = args.robot
+    arch_thr  = args.arc_thr
+    knn       = args.knn
+    brange    = args.brange
+    popSize   = args.popsize
+    numGen    = args.numgen
+    internal_novelty = args.internal_novelty
+
+    parents = POPULATION(popSize, robotType)
+
+    if internal_novelty:
+        parents.Evaluate_Internal_Novelty(False, True, brange)
+
+    else:
+        parents.Evaluate_External_Novelty(False, True, knn)
+        parents.Update_Archive(arch_thr)
+
+    for g in range(1, numGen):
+
+        children = deepcopy(parents)
+        children.Mutate()
+
+        if internal_novelty:
+
+            children.Evaluate_Internal_Novelty(False, True, brange)
+            parents.ReplaceWith(children)
+
+        else:
+            children.Evaluate_External_Novelty(False, True, knn)
+            parents.ReplaceWith(children)
+            parents.Evaluate_External_Novelty(False, True, knn)
+            parents.Update_Archive(arch_thr)
+
+        print g,
+        parents.Print()
+        print
+
+        if not internal_novelty:
+            parents.Print_Archive()
+
+    if internal_novelty:
+        parents.Store_All_Above_Average()
+
+    else:
+        parents.Store_Archive()
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Diversity pool using either intenal or external diversity.')
+    
+    parser.add_argument('--robot', type = str, default='1', help='Morphology types: {1,2,3,4,spherebot\
+        ,crabbot, quadruped, shinbot, snakebot}, default=1')
+
+    parser.add_argument('--popsize', type = int, default=30, help=\
+        'Size of populaiton, default=30.')
+    
+    parser.add_argument('--numgen', type = int, default=100, help=\
+        'Number of generations, default=100.')
+
+    parser.add_argument('--arc_thr', type = float, default=4.0,
+        help='An individual with a fitness larger than this value is inserted into the archive, default=4.0.')
+
+    parser.add_argument('--knn', type=int, default=3,
+        help='K nearest neighbor for calculating fitness in external novelty, default=3.')
+
+    parser.add_argument('--brange', type=int, default=10,
+        help='Divide [0, 1] in brange values for word to vector, default=10.')
+
+    parser.add_argument('--internal_novelty', action='store_true',
+        help='A boolean flag, default=False')
+
+    args = parser.parse_args()
+
+    main(args)
+
--- pyrosim/population.py
+++ pyrosim/population.py
@@ -17,16 +17,14 @@ class POPULATION:
     def __init__(self, ps, robotType):
 
         self.p = {}
-
         self.popSize = ps
 
         for i in range(0, self.popSize):
-
             self.p[i] = INDIVIDUAL(i, robotType) 
 
     def Compute_External_Novelty(self):
 
-        self.novelty = np.zeros((self.popSize,self.popSize + POPULATION.asize))
+        self.novelty = np.zeros((self.popSize, self.popSize + POPULATION.asize))
 
         # distance to all the other individuals
         for i in range(0, self.popSize):
@@ -34,37 +32,27 @@ class POPULATION:
             self.novelty[i][i] = float('inf')
 
             for j in range(i+1, self.popSize):
-
                 vec1 = self.p[i].head_trajectory
-
                 vec2 = self.p[j].head_trajectory
-
                 dist = LA.norm(vec1-vec2)
-
                 self.novelty[i][j] = self.novelty[j][i] = dist
 
         # distance to all the individuals in archive
         for i in range(0, self.popSize):
 
             for j in range(0, POPULATION.asize):
-
                 vec1 = self.p[i].head_trajectory
-
                 vec2 = POPULATION.archive[j].head_trajectory
-
                 dist = LA.norm(vec1-vec2)
-
                 self.novelty[i][self.popSize+j] = dist
-
-            
+    
     def Print(self):
 
         for i in self.p:
-
             self.p[i].Print()
         print
 
-    def Evaluate_External_Novelty(self, pp, pb, brange=10, knn=5):
+    def Evaluate_External_Novelty(self, pp, pb, knn=5):
 
         for i in self.p:
             self.p[i].Start_Evaluate(pp, pb, (c.NUM_BIAS_NEURONS+c.NUM_COMMAND_NEURONS)*[1.0])
@@ -79,40 +67,35 @@ class POPULATION:
             idx = np.argpartition(self.novelty[i], knn)
             self.p[i].fitness = np.average(self.novelty[i][idx[:knn]])
 
-    def Evaluate_Internal_Novelty(self, pp, pb, brange=3, knn=5):
+    def Evaluate_Internal_Novelty(self, pp, pb, brange=3):
 
-        temp={}
+        temp = {}
 
-        for i in self.p: temp[i]=[]
+        for i in self.p: 
+            temp[i]=[]
 
         for b in np.linspace(0, 1, brange):
 
             for i in self.p:
-
-                self.p[i].Start_Evaluate(pp, pb, c.NUM_BIAS_NEURONS*[1.0]+[b] if c.NUM_BIAS_NEURONS>0 else [b])
+                self.p[i].Start_Evaluate(pp, pb, c.NUM_BIAS_NEURONS*[1.0]+[b]\
+                 if c.NUM_BIAS_NEURONS>0 else [b])
 
             for i in self.p:
-
                 self.p[i].Get_Head_Trajectory()
             
             for i in self.p:
-
                 temp[i].append(self.p[i].head_trajectory)
 
         for i in self.p:
 
             novelty = np.zeros((brange, brange))
-
             for j in range(0, brange):
 
                 novelty[j][j] = float('inf')
-
                 for k in range(j+1, brange):
-
                     novelty[k][j] = novelty[j][k] = LA.norm(temp[i][j] - temp[i][k])
 
             self.p[i].fitness = np.min(novelty)
-
 
     def Print_Archive(self):
 
@@ -121,9 +104,7 @@ class POPULATION:
     def Unique(self, i):
 
         for a in range(0, POPULATION.asize):
-
             if ( POPULATION.archive[a].head_trajectory == self.p[i].head_trajectory).all():
-
                 return False
 
         return True
@@ -135,9 +116,7 @@ class POPULATION:
             if self.p[i].fitness > threshold and self.Unique(i):
 
                 POPULATION.archive[POPULATION.asize] = deepcopy(self.p[i])
-
                 POPULATION.archive[POPULATION.asize].id = POPULATION.asize
-
                 POPULATION.asize = POPULATION.asize + 1
 
     def Mutate(self):
