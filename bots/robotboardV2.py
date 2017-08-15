b88f86e84416bc1c3bbb1fa511fb154ef04db141
TwitchPlaysRobotics <twitchplaysrobotics@ip138119.uvm.edu>
2017-08-15 10:23:03 -0400

    change leaderboard and clean up

---
+++ bots/robotboardV2.py
@@ -0,0 +1,112 @@
+from pygameWrapper import PYGAMEWRAPPER
+import time
+import random
+from database import DATABASE
+import pygame
+from timer import TIMER
+import numpy as np
+import datetime
+
+DB = DATABASE()
+WIDTH     = 900
+HEIGHT    = 200
+FONT_SIZE = 23
+UPDATE_PERIOD = 2
+BLINK_PERIOD = 10
+DRAW_PERIOD  = 30
+WSPACE = 85
+#get screen
+WINDOW = PYGAMEWRAPPER(width = WIDTH, height = HEIGHT, title="Robot's Information",
+                     fontSize = FONT_SIZE)
+SCREEN = WINDOW.screen
+BG_COLOR = (30, 144, 255)
+#create new table object
+updateTimer = TIMER(UPDATE_PERIOD)
+blinkTimer  = TIMER(BLINK_PERIOD)
+
+prev_cmd = ""
+
+def Draw_Robot_Window( robotInfo ):
+
+    global prev_cmd
+
+    # print ('current robot info: ', robotInfo)
+    if robotInfo == None: return None
+
+    allTypes = {'1':'stickbot', '2': 'twigbot', '3':'branchbot', '4': 'treebot',\
+    'quadruped':'quadruped', 'shinbot': 'tablebot', 'crabbot': 'crabbot',\
+     'starfishbot':'starfishbot', 'spherebot':'spherebot', 'snakebot':'snakebot',\
+     'snakeplusbot':'snakeplusbot', 'humanoid': 'humanoid', 'crabplusbot':'crabplusbot',\
+     'quadrupedplus':'quadrupedplus'}
+
+    typeKey    = robotInfo['robotType']
+    robotType  = allTypes[typeKey]
+
+    robotID    = robotInfo['robotID']
+    color      = robotInfo['color']
+    numOfKind  = robotInfo['numOfKind'] if robotInfo['numOfKind'] != None else 0
+    numYes     = robotInfo['numYes'] if robotInfo['numYes'] != None else 0
+    numNo      = robotInfo['numNo'] if robotInfo['numNo'] != None else 0
+    numLike    = robotInfo['numLike'] if robotInfo['numLike'] != None else 0
+    numDislike = robotInfo['numDislike'] if robotInfo['numDislike'] != None else 0
+    cmdTxt     = robotInfo['cmdTxt']
+
+    dt = datetime.datetime.now() - robotInfo['birthDate']
+
+    minute, second = divmod(dt.days*86400 + dt.seconds, 60)
+    hour, minute   = divmod(minute, 60)
+    day, hour      = divmod(hour, 24)
+
+    age = "%dd %dh:%02dm:%02ds"%(day, hour, minute, second)
+
+    myy = 10
+    WINDOW.Draw_Text("This robot is trying to ", x=5, y=myy, color='WHITE', fontSize=30)
+
+    if cmdTxt != prev_cmd:
+        blinkTimer.Reset()
+
+    if not blinkTimer.Time_Elapsed():
+        WINDOW.Draw_Text(cmdTxt ,x=WINDOW.text_x+WINDOW.text_width,\
+         y=myy, color='YELLOW', bold=True, underline=False, fontSize=35)
+
+    else:
+
+        WINDOW.Draw_Text(cmdTxt ,x=WINDOW.text_x+WINDOW.text_width,\
+         y=myy, color='WHITE', bold=True, underline=False, fontSize=30)
+
+    WINDOW.Draw_Text("Need help? Type ?", x=675, y=myy, color='WHITE',
+                     fontSize=FONT_SIZE+5)
+
+    myy += 40
+    WINDOW.Draw_Text("If you think it is, type !"+color+"y. ("+str(numYes)+")", 
+                    x=5, y=myy, color='WHITE', fontSize=FONT_SIZE)
+    myy += 40
+    WINDOW.Draw_Text("If not, type !"+color+"n.                ("+str(numNo)+")",
+                    x=5, y=myy, color='WHITE', fontSize=FONT_SIZE)
+    myy += 40
+    WINDOW.Draw_Text("If you like this robot, type !"+color+"l. ("+str(numLike)+")",
+                    x=5, y=myy, color='WHITE', fontSize=FONT_SIZE)
+    myy += 40
+    WINDOW.Draw_Text("If you dislike it, type !"+color+"d.        ("+str(numDislike)+")",
+                    x=5, y=myy, color='WHITE', fontSize=FONT_SIZE)
+
+    prev_cmd = cmdTxt
+
+robotInfo = DB.Fetch_Robot_Information()
+ 
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            WINDOW.Quit()
+                        
+    WINDOW.Wipe(BG_COLOR)    
+
+    if updateTimer.Time_Elapsed():
+
+        # fetch from database
+        robotInfo = DB.Fetch_Robot_Information()
+        updateTimer.Reset()
+
+    Draw_Robot_Window(robotInfo)
+    WINDOW.Refresh()
--- pyrosim/constants.py
+++ pyrosim/constants.py
@@ -1,55 +1,14 @@
 L = 0.1
-
 R = L/5
-
 PI = 3.14159
-
 headRadius = 2 * R
-
 eyeRadius = 0.015
-
 popSize = 30
-
 numEnvs = 2
-
-# ------------- Things to maximize -----------------
-
-maximizeTouch = 'touch'
-
-maximizeProprioception = 'proprioception'
-
-maximizeHeight = 'height'
-
-maximizeDistance = 'distance'
-
-maximizeLight = 'light'
-
-maximizeRed = 'red'
-
-maximizeGreen = 'green'
-
-maximizeBlue = 'blue'
-
-maximizeWhite = 'white'
-
-maximizeVestibular = 'vestibular'
-
-maximizeMovement = 'movement'
-
-maximizePushing = 'pushing'
-
-maximizeMovementAndHeight = 'movement_height'
-
-maximizeDistanceAndHeight = 'distance_height'
-
-maximizeTouch = 'touch'
 
 # ------------- Other parameters -------------------
 
-evaluationTime = 600
-
-# maxDepth = 2
-
+evaluationTime = 1800
 maxChildren = 2
 
 length = 0.5 / 4.0
@@ -67,12 +26,9 @@ JOINT_ANGLE_MUTATION_MAGNITUDE = 3.14159/8.0
 # ----------- Neural network parameters ------------
 
 SENSOR_NEURON = 0
-
 HIDDEN_NEURON = 1
-
-MOTOR_NEURON = 2
-
-BIAS_NEURON = 3
+MOTOR_NEURON  = 2
+BIAS_NEURON   = 3
 
 POS_SENSOR = 4
 PRO_SENSOR = 5
@@ -80,10 +36,8 @@ RAY_SENSOR = 6
 TOC_SENSOR = 7
 LIT_SENSOR = 8
 
-NUM_HIDDEN_NEURONS = 5
-
+NUM_HIDDEN_NEURONS  = 5
 NUM_COMMAND_NEURONS = 1
-
-NUM_BIAS_NEURONS = 1
+NUM_BIAS_NEURONS    = 1
 
 TAU_MAX = 1.0
