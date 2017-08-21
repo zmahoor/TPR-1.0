e560b4ed4e1a958955a81621e02bba9dcb804e1b
TwitchPlaysRobotics <twitchplaysrobotics@ip138119.uvm.edu>
2017-08-21 18:25:21 -0400

    more charts

--- analysis/TPR_Figure_Robots-master/fig.py
+++ analysis/TPR_Figure_Robots-master/fig.py
@@ -6,7 +6,8 @@ import numpy as np
 csv = pd.read_csv("data.csv", index_col=0)
 csv = csv.transpose()
 
-mycolor = ['#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061']
+mycolor = ['#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#d1e5f0','#92c5de',
+            '#4393c3','#2166ac','#053061']
 
 csv.plot.bar(stacked=True, figsize=(12, 8), color=mycolor)
 plt.title('Number of robots in each species', fontsize=20)
