573620c919d358e15a919731cdef0ceea4058642
twitchplaysrobotics <zmahoor@vols.utk.edu>
2017-10-03 09:06:47 -0400

    critic results

---
+++ analysis/Robots_Commands_Disagreements.py
@@ -0,0 +1,201 @@
+import sys 
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+from collections import namedtuple, Counter
+import matplotlib.pyplot as plt
+import pandas as pd
+sys.path.append('../bots')
+from database import DATABASE
+
+db = DATABASE()
+
+names = {'1':'stickbot', '2': 'twigbot', '3':'branchbot', '4': 'treebot',
+    'quadruped':'quadruped', 'starfishbot':'starfishbot', 'spherebot':'spherebot', 
+    'shinbot': 'tablebot', 'snakebot':'snakebot', 'crabbot': 'crabbot', 
+    'snakeplusbot':'snakebot+', 'humanoid': 'humanoid', 'crabplusbot':'crabbot+',
+     'quadrupedplus':'quadruped+'}
+
+sql="""SELECT d.displayID, d.robotID, userName, reward, r.timeArrival, d.cmdTxt, ro.type
+FROM TwitchPlays.display as d join TwitchPlays.reward_log as r on d.displayID=r.displayID 
+join TwitchPlays.robots  as ro on ro.robotID=d.robotID where r.reward='y' or r.reward='n'
+order by r.timeArrival ASC;"""
+records = db.Execute_Select_Sql_Command(sql, ' ')
+
+displays_dict, command_robot = {}, {}
+robot_disagreements, command_disagreements, robot_command_disagreements = Counter(), Counter(), Counter()
+robot_agreements, command_agreements, robot_command_agreements = Counter(), Counter(), Counter()
+num_disagreements, num_agreements = 0, 0
+
+for record in records:
+    displayID, userName, reward = record['displayID'], record['userName'], record['reward']
+    command_robot[displayID] = (record['type'], record['cmdTxt'])
+
+    if displayID not in displays_dict: displays_dict[displayID] = set()
+    displays_dict[displayID].add((userName, reward))
+
+#delete all the evaluations with only one feedback
+for key, val in displays_dict.items(): 
+    if len(val) == 1: del displays_dict[key]
+
+# iterate over the rest and find the total number of agreements, total number of disagreements
+# agreements/disagreements per robot/command
+for key, val in displays_dict.items(): 
+    feedback = set([v[1] for v in val])
+    robot, command = command_robot[key]
+
+    if len(feedback) == 2: 
+        num_disagreements += 1
+        robot_disagreements[robot] += 1
+        command_disagreements[command] += 1
+        robot_command_disagreements[(robot, command)] += 1
+
+    else:
+        num_agreements += 1
+        robot_agreements[robot] += 1
+        command_agreements[command] += 1
+        robot_command_agreements[(robot, command)] += 1
+
+print "Total displays with agreements: {}.".format(num_agreements)
+print "Total displays with disagreements: {}.".format(num_disagreements)
+
+#calcuate the total number of evaluations with at least two feedback
+#1) for each species
+#2) for each command
+#3) for pair of (robot, command)
+robots_total = (robot_agreements + robot_disagreements)
+commands_total = (command_agreements + command_disagreements)
+robot_command_total = (robot_command_agreements + robot_command_disagreements)
+
+#write the most common robot command pairs to a file for a heat map
+outfile = open("disagreement_data.csv", "w")
+writer = csv.writer(outfile, delimiter=",")
+header = ['commands']
+
+for cmd in command_disagreements.most_common(10): header.append(cmd[0])
+writer.writerow(header)
+
+for robot in robot_disagreements.most_common(10):
+    row = [names[robot[0]]]
+    for cmd in command_disagreements.most_common(10):
+        if float(robot_command_total[(robot[0], cmd[0])]) != 0:
+            row.append(robot_command_disagreements[(robot[0], cmd[0])]/float(robot_command_total[(robot[0], cmd[0])]))
+        else:
+            print "No data: ", (robot[0], cmd[0])
+            row.append(-0.0)
+    writer.writerow(row)
+
+outfile.close()
+
+########################################################################################################################
+# plot a heat map
+csv = pd.read_csv("disagreement_data.csv", index_col=0)
+print csv
+fig, ax  = plt.subplots()
+heatmap  = ax.pcolor(csv, cmap=plt.cm.Blues, alpha=0.8)
+fig = plt.gcf()
+fig.set_size_inches(9, 11)
+
+ax.set_frame_on(False)
+ax.set_yticks(np.arange(csv.shape[0]) + 0.5, minor=False)
+ax.set_xticks(np.arange(csv.shape[1]) + 0.5, minor=False)
+ax.invert_yaxis()
+ax.xaxis.tick_top()
+labels = list(csv.columns.values)
+ax.set_xticklabels(labels, minor=False)
+ax.set_yticklabels(csv.index, minor=False)
+
+# plt.xticks(rotation=90)
+
+# plt.title('More blue = more obedient\n\n\n')
+plt.xlabel('Commands (More blue = more Disagreements)')
+plt.ylabel('Species')
+ax.grid(False)
+ax = plt.gca()
+
+for t in ax.xaxis.get_major_ticks():
+    t.tick1On = False
+    t.tick2On = False
+for t in ax.yaxis.get_major_ticks():
+    t.tick1On = False
+    t.tick2On = False
+
+plt.show()
+########################################################################################################################
+
+#write the most common robot command pairs to a file for a heat map
+# outfile = open("agreement_data.csv", "w")
+# writer  = csv.writer(outfile, delimiter=",")
+# header = ['commands']
+#
+# for cmd in command_agreements.most_common(10): header.append(cmd[0])
+# writer.writerow(header)
+#
+# for robot in robot_agreements.most_common(10):
+#     row = [names[robot[0]]]
+#     for cmd in command_agreements.most_common(10):
+#         if float(robot_command_total[(robot[0], cmd[0])]) != 0:
+#             row.append(robot_command_agreements[(robot[0], cmd[0])]/float(robot_command_total[(robot[0], cmd[0])]))
+#         else:
+#             print "No data: ", (robot[0], cmd[0])
+#             row.append(-0.0)
+#     writer.writerow(row)
+#
+# outfile.close()
+#
+# ########################################################################################################################
+# # plot a heat map
+# csv = pd.read_csv("agreement_data.csv", index_col=0)
+# print csv
+# fig, ax  = plt.subplots()
+# heatmap  = ax.pcolor(csv, cmap=plt.cm.Blues, alpha=0.8)
+# fig = plt.gcf()
+# fig.set_size_inches(9, 11)
+#
+# ax.set_frame_on(False)
+# ax.set_yticks(np.arange(csv.shape[0]) + 0.5, minor=False)
+# ax.set_xticks(np.arange(csv.shape[1]) + 0.5, minor=False)
+# ax.invert_yaxis()
+# ax.xaxis.tick_top()
+# labels = list(csv.columns.values)
+# ax.set_xticklabels(labels, minor=False)
+# ax.set_yticklabels(csv.index, minor=False)
+#
+# # plt.xticks(rotation=90)
+#
+# plt.xlabel('Commands (More blue = more Agreements)')
+# plt.ylabel('Species')
+# ax.grid(False)
+# ax = plt.gca()
+#
+# for t in ax.xaxis.get_major_ticks():
+#     t.tick1On = False
+#     t.tick2On = False
+# for t in ax.yaxis.get_major_ticks():
+#     t.tick1On = False
+#     t.tick2On = False
+#
+# plt.show()
+########################################################################################################################
+
+# print
+# print robot_agreements
+# print robot_disagreements
+# print robots_total
+#
+# agr_list, dis_list, labels = [], [], []
+#
+# for key in robot_agreements.keys():
+#     if key not in ('humanoid', 'crabplusbot', 'quadrupedplus', 'snakeplusbot'):
+#         agr_list.append(float(robot_agreements[key])/robots_total[key])
+#         dis_list.append(float(robot_disagreements[key])/robots_total[key])
+#         labels.append(names[key])
+#
+# p1 = plt.bar(range(len(agr_list)), agr_list, color='b')
+# p2 = plt.bar(range(len(agr_list)), dis_list, color='r', bottom=agr_list)
+# plt.xlabel('Species')
+# plt.ylabel('Ratio', fontsize=14)
+# plt.title('Species vs. Agreements/Disagreements')
+# plt.xticks(np.arange(0.5, len(agr_list)+0.5, 1.0), labels)
+# plt.legend((p1[0], p2[0]), ('Agreements', 'Disagreements'))
+# plt.show()
--- critic/critic_results.py
+++ critic/critic_results.py
@@ -1,49 +1,7 @@
-import sys 
+import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.stats.stats import ttest_ind
-
-# command = 'move'
-
-# regular_data = {'starfishbot':[0.151, 0.035], 'branchbot':[0.286, 0.091]}
-# permuted_data = {'starfishbot':[0.242, 0.088], 'branchbot':[0.384, 0.091]}
-# random_model = {'starfishbot':[0.817, 0.082], 'branchbot':[0.459, 0.110]}
-#
-# fig, ax = plt.subplots()
-#
-# x = [.15, .25, .35, .55, .65, .75]
-#
-# point0 = ax.errorbar(x[2], regular_data['starfishbot'][0],
-#             yerr = regular_data['starfishbot'][1],
-#             fmt = 'o', markersize = 5, color = 'g', ecolor = 'g')
-# point1 = ax.errorbar(x[1], permuted_data['starfishbot'][0],
-#             yerr = permuted_data['starfishbot'][1],
-#             fmt = 'o', markersize = 5, color = 'r', ecolor = 'r')
-# point2 = ax.errorbar(x[0], random_model['starfishbot'][0],
-#             yerr = random_model['starfishbot'][1],
-#             fmt = 'o', markersize = 5, color = 'b', ecolor = 'b')
-#
-# point3 = ax.errorbar(x[5], regular_data['branchbot'][0],
-#             yerr = regular_data['branchbot'][1],
-#             fmt = 'o', markersize = 5, color = 'g', ecolor = 'g')
-# point4 = ax.errorbar(x[4], permuted_data['branchbot'][0],
-#             yerr = permuted_data['branchbot'][1],
-#             fmt = 'o', markersize = 5, color = 'r', ecolor = 'r')
-# point5 = ax.errorbar(x[3], random_model['branchbot'][0],
-#             yerr = random_model['branchbot'][1],
-#             fmt = 'o', markersize = 5, color = 'b', ecolor = 'b')
-#
-# ax.set_ylabel("Error")
-# ax.set_xticks(x)
-# ax.set_xticklabels(["", "Starfishbot", "", "", "Branchbot", ""])
-# ax.set_title("Mean Error of Predictive Model")
-# ax.legend((point0[0], point1[0], point2[0]), ("Experiment", "Permuted Control", "Random Control"),
-#         loc = 9,  numpoints = 1, fontsize = 10)
-# plt.xlim((x[0] - .1, x[len(x) -1] + .1))
-# plt.ylim((.395, .53))
-
-# plt.show()
-########################################################################################################################
 
 def stars(p):
    if p < 0.0001:
@@ -57,23 +15,10 @@ def stars(p):
    else:
        return "-"
 
-# command = ['move', 'stop']
-# regular_data = {'starfishbot':[0.197, 0.043], 'branchbot':[0.350, 0.074], 'snakebot':[0.387, 0.043], 'tablebot':[0.212, 0.073],
-#                'quadruped':[0.396, 0.062], 'treebot':[0.292, 0.066], 'spherebot':[0.411, 0.057], 'stickbot':[0.419, 0.063],
-#                 'twigbot':[0.370, 0.107]}
-# permuted_data = {'starfishbot':[0.491, 0.065], 'branchbot':[0.470, 0.083], 'snakebot':[0.488, 0.037], 'tablebot':[0.478, 0.097],
-#                 'quadruped':[0.474, 0.065], 'treebot':[0.452, 0.104], 'spherebot':[0.486, 0.069], 'stickbot':[0.488, 0.088],
-#                 'twigbot':[0.494, 0.105]}
-# # random_model = {'starfishbot':[0.5, 0.059], 'branchbot':[0.538, 0.148], 'snakebot':[0.482, 0.077], 'tablebot':[0.490, 0.094],
-# #                 'quadruped':[0.428, 0.133], 'treebot':[], 'spherebot':[]}
-#
-# regular_permuted_p_value = {'starfishbot': 5.3238905170342945e-28, 'branchbot':3.0461481443595506e-07, 'snakebot':1.4964029414300558e-13,
-#                             'tablebot':4.466754369482279e-17, 'quadruped':1.6739299729529018e-05, 'treebot': 2.9136521042000613e-09,
-#                             'spherebot':2.7774022198088076e-05, 'stickbot':0.0010708904740438063, 'twigbot':4.166608497071619e-05}
 
+regular = pd.read_csv("/Users/twitchplaysrobotics/TPR-1.0/critic/critic_results_regular_150.csv", index_col=0).transpose()
+permuted = pd.read_csv("/Users/twitchplaysrobotics/TPR-1.0/critic/critic_results_permuted_150.csv", index_col=0).transpose()
 
-regular = pd.read_csv("critic_results_regular.csv", index_col=0).transpose()
-permuted = pd.read_csv("critic_results_permuted.csv", index_col=0).transpose()
 regular_mean, regular_std = regular.mean(), regular.std()
 permuted_mean, permuted_std = permuted.mean(), permuted.std()
 
@@ -113,5 +58,5 @@ ax.set_xticklabels(x_ticks)
 ax.set_title("Mean Error of Predictive Model Across 30 Trials for each Species")
 ax.legend((points[0], points[1]), ("Experiment", "Permuted Control"), loc=2,  numpoints=1, fontsize=14)
 plt.xlim((x[0] - .1, x[len(x) - 1] + .1))
-plt.ylim((.1, .7))
+plt.ylim((.05, .7))
 plt.show()
